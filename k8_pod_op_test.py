ffd788f9f9f6d9b0aaed9f78154adc3db0a51124
Kyle Lawrence <kylelawrence@Aspens-MacBook-Air.local>
2024-03-22 19:27:16 -0700

    add with DAG statement to k8 posd tester

--- k8_pod_op_test.py
+++ k8_pod_op_test.py
@@ -1,14 +1,22 @@
+from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 
-k = KubernetesPodOperator(
-    name="hello-dry-run",
-    image="debian",
-    cmds=["bash", "-cx"],
-    arguments=["echo", "10"],
-    labels={"foo": "bar"},
-    task_id="dry_run_demo",
-    tags = ['test'],
-    do_xcom_push=True
-)
+with DAG(
+    "hello-dry-run",
+    description='Runs bash command pip list',
+    tags=['example']
+) as dag:
 
-k.dry_run()
+
+    k = KubernetesPodOperator(
+        name="hello-dry-run",
+        image="debian",
+        cmds=["bash", "-cx"],
+        arguments=["echo", "10"],
+        labels={"foo": "bar"},
+        task_id="dry_run_demo",
+        tags = ['test'],
+        do_xcom_push=True
+    )
+
+    k.dry_run()
