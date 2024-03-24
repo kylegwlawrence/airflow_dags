0f31c962e6ab4c3f0c1b680445adf85c358f9470
Kyle Lawrence <kylelawrence@Aspens-MacBook-Air.local>
2024-03-23 19:50:01 -0700

    add n_element to params in dag that is passable at runtime

--- docker_in_kop_test.py
+++ docker_in_kop_test.py
@@ -7,13 +7,15 @@ dag_name = f"docker-python-alpine"
 with DAG(
     dag_id = dag_name,
     description='testing a simple python script in a docker container with an argument passed in',
-    tags=['test']
+    tags=['test'],
+    params = {'n_elements': Param(110, type="integer")},
+    render_template_as_native_obj=True # renders the n_elements param as an integer when passed to the task
 ) as dag:
 
     run_docker = KubernetesPodOperator(
         name="run_docker",
         image=image_name,
-        cmds=["python", "main.py", "--n_elements", "116"],
+        cmds=["python", "main.py", "--n_elements", "{{ params.my_int_param }}"],
         task_id="run_docker",
         in_cluster=True,
         do_xcom_push=True,
